aa54081b5963e84056126733657f942c67d73a17
MatthewPindral <mpindral@deakin.edu.au>
2018-03-26 22:23:40 +1100

    Again
    Again

---
+++ sit305_ass/Assets/AssetsCredits/CRedits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRedits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //http://d-maps.com/carte.php?num_car=3128&lang=en
+}
